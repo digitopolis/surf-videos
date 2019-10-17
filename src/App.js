778070d085b280080115765ed7409c232c1446e4
Matt Readout <matt.readout@gmail.com>
2019-10-17 11:48:23 -0400

    make app class component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import { apiKey, SURFVIDEOS } from './apiEndpoints'
 import './App.css';
+import Container from 'react-bootstrap/Container'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	render() {
+	  return (
+			<Container>
+				<h1>videos here</h1>
+			</Container>
+	  );
+	}
 }
 
 export default App;
