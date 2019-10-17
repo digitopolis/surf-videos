9159d90a2208094eb2df547fe2f6c02acdc8bc06
Matt Readout <matt.readout@gmail.com>
2019-10-17 11:52:25 -0400

    fetch videos on load, store in state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import { apiKey, SURFVIDEOS } from './apiEndpoints'
+import { SURFVIDEOS } from './apiEndpoints'
 import './App.css';
 import Container from 'react-bootstrap/Container'
 
 class App extends React.Component {
+
+	state = {
+		searchResults: []
+	}
+
+	async componentDidMount() {
+		const data = await fetch(SURFVIDEOS).then(res => res.json())
+		this.setState({
+			searchResults: data.items
+		})
+	}
+
 	render() {
 	  return (
 			<Container>
