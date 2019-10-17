66fe1a7f220c1dc5baaf9ffc7f0aad5b46ba5256
Matt Readout <matt.readout@gmail.com>
2019-10-17 11:42:55 -0400

    define api endpoints/key

---
+++ src/apiEndpoints.js
@@ -0,0 +1,5 @@
+// api key from original repo
+export const apiKey = 'AIzaSyBeimXtjgzfQcogY-fP8_CHPybmLpFaieo'
+
+// get 25 surf video resources
+export const SURFVIDEOS = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&part=snippet&type=video&maxResults=25&q=surf`
