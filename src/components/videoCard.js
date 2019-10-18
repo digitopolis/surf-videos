3713dcedeb0991870a3d55c0216123dd9986d7b9
Matt Readout <matt.readout@gmail.com>
2019-10-18 14:41:01 -0400

    videoList and card styling

--- src/components/videoCard.js
+++ src/components/videoCard.js
@@ -2,14 +2,14 @@ import React from 'react'
 import Card from 'react-bootstrap/Card'
 
 const VideoCard = ({ snippet }) => {
-	const thumbnailUrl = snippet.thumbnails.default.url
+	const thumbnailUrl = snippet.thumbnails.medium.url
 	return (
 		<Card>
 			<Card.Img variant='top' src={thumbnailUrl} />
 			<Card.Body>
 				<Card.Title>{snippet.title}</Card.Title>
 				<Card.Subtitle className='mb-2 text-muted'>{snippet.channelTitle}</Card.Subtitle>
-				<Card.Text>{snippet.description}</Card.Text>
+				<Card.Text><small>{snippet.description}</small></Card.Text>
 			</Card.Body>
 		</Card>
 	)
--- src/containers/videoList.js
+++ src/containers/videoList.js
@@ -1,14 +1,17 @@
 import React from 'react'
+import Col from 'react-bootstrap/Col'
 import CardColumns from 'react-bootstrap/CardColumns'
 import VideoCard from '../components/videoCard'
 
 const VideoList = ({ videos }) => {
 	return (
-		<CardColumns>
-			{videos.map(video => {
-				return <VideoCard {...video} />
-			})}
-		</CardColumns>
+		<Col>
+			<CardColumns>
+				{videos.map(video => {
+					return <VideoCard {...video} />
+				})}
+			</CardColumns>
+		</Col>
 	)
 }
 
