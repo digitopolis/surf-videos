ae02599a36bd56c199e0df4fe1f9d06cb12c3cbc
Matt Readout <matt.readout@gmail.com>
2019-10-19 13:22:07 -0400

    add button to load more videos

--- src/containers/videoList.js
+++ src/containers/videoList.js
@@ -1,19 +1,24 @@
 import React from 'react'
 import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import Button from 'react-bootstrap/Button'
 import CardColumns from 'react-bootstrap/CardColumns'
 import VideoCard from '../components/videoCard'
 
 const VideoList = ({ videos, handleVideoSelect }) => {
 	return (
-		<Col>
+		<Col md='auto'>
 			<CardColumns>
 				{videos.map(video => {
 					return <VideoCard {...video}
-										key={video.id}
+										key={video.id.videoId}
 										handleVideoSelect={handleVideoSelect}
 								 />
 				})}
 			</CardColumns>
+			<Row className='justify-content-center my-2'>
+				<Button variant='secondary'>Load more</Button>
+			</Row>
 		</Col>
 	)
 }
